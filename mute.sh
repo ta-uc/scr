ddd7230432f8eadf2c74c30106b20e44698dcf57
Takumi Uchida <mitauchiku@gmail.com>
2020-05-15 09:24:39 +0900

    update mute script

--- mute.sh
+++ mute.sh
@@ -5,7 +5,7 @@ CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
 if [[ $CURRENT_STATE == '[on]' ]]; then
     amixer set Master mute
 else
+    amixer set Speaker unmute
     amixer set Master unmute
-    amixer set Front unmute
     amixer set Headphone unmute
 fi
