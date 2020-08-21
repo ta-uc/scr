d307735be3fcd3b01132fc797f7eeb181aa56932
Takumi Uchida <mitauchiku@gmail.com>
2020-08-22 00:18:46 +0900

    mod: output as variable

--- display.sh
+++ display.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $(echo -e "Main\nMirror\nExtend"|dmenu -i -p "Display?") in 
-    "Main") xrandr --output eDP1 --mode 1920x1080 --panning 1920x1080* --primary --output HDMI2 --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Mirror") xrandr --output eDP1 --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output HDMI2 --mode 1920x1080 --same-as eDP1 && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Extend") xrandr --output eDP1 --mode 1600x900 --primary --output HDMI2 --auto --right-of eDP1 && feh --bg-fill ~/Pictures/WP/w.jpg;;
+  "Main") xrandr --output $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --panning 1920x1080* --primary --output HDMI2 --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Mirror") xrandr --output  $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output HDMI2 --mode 1920x1080 --same-as $(xrandr | grep primary | awk '{print $1}') && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Extend") xrandr --output $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --primary --output HDMI2 --auto --right-of $(xrandr | grep primary | awk '{print $1}') && feh --bg-fill ~/Pictures/WP/w.jpg;;
 esac
 
