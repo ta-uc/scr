8e023d50a75965f9ff4c44b6a4d3aae83f80e7b2
Takumi Uchida <mitauchiku@gmail.com>
2020-08-25 00:19:50 +0900

    mod: clean syntax with variable

--- display.sh
+++ display.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PRIMARY_DISPLAY=$(xrandr | grep primary | awk '{print $1}')
 case $(echo -e "Main\nMirror\nExtend"|dmenu -i -p "Display?") in 
-  "Main") xrandr --output $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --panning 1920x1080* --primary --output HDMI2 --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Mirror") xrandr --output  $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output HDMI2 --mode 1920x1080 --same-as $(xrandr | grep primary | awk '{print $1}') && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Extend") xrandr --output $(xrandr | grep primary | awk '{print $1}') --mode 1920x1080 --primary --output HDMI2 --auto --right-of $(xrandr | grep primary | awk '{print $1}') && feh --bg-fill ~/Pictures/WP/w.jpg;;
+  "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output HDMI2 --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output HDMI2 --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Extend") xrandr --output $PRIMARY_DISPLAY  --mode 1920x1080 --primary --output HDMI2 --auto --right-of $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
 esac
 
