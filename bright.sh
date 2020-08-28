7d9bdbfa42ac8e41479fe584170dab94f820d5d7
Takumi Uchida <mitauchiku@gmail.com>
2020-08-29 00:24:15 +0900

    mod: show in progress bar

--- bright.sh
+++ bright.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-cb=`cat /sys/class/backlight/intel_backlight/brightness`
-if [ "$1" -eq "1" ];
-then
-    newb=`expr $cb + 85`
-    echo $newb | sudo tee /sys/class/backlight/intel_backlight/brightness
-elif [ "$1" -eq "0" ];
-then
-    newb=`expr $cb - 85`
-    echo $newb | sudo tee /sys/class/backlight/intel_backlight/brightness
+if [ "$1" -eq "1" ]; then
+    xbacklight -inc 10
+elif [ "$1" -eq "0" ]; then
+    xbacklight -dec 10
 fi
-
-notify-send -t 100 $(seq -s▪ $(( ($newb*30/7585)+1 )) |tr -d '[:digit:]')$(seq -s_ $(( 30-($newb*30/7585) )) |tr -d '[:digit:]')
-
+NEWB_RATE=$(xbacklight -get|xargs printf "%.0f")
+NOTI_ID=$(notify-send.py "BRIGHTNESS" \
+                         --hint string:image-path:video-display int:value:$NEWB_RATE \
+                         --replaces-process "brightness-popup")
