e1aebac11e07842cf4e34f96f0db293d8fd68cd5
Takumi Uchida <mitauchiku@gmail.com>
2020-08-30 22:53:09 +0900

    mod: lower update step

--- bright.sh
+++ bright.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" -eq "1" ]; then
-    xbacklight -inc 10
+    xbacklight -inc 5
 elif [ "$1" -eq "0" ]; then
-    xbacklight -dec 10
+    xbacklight -dec 5
 fi
 NEWB_RATE=$(xbacklight -get|xargs printf "%.0f")
 NOTI_ID=$(notify-send.py "BRIGHTNESS" \
