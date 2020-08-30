64c8c1bfdfc400879fd45803ea657b8771d35988
Takumi Uchida <mitauchiku@gmail.com>
2020-08-30 22:53:37 +0900

    mod: use rofi and zenify

--- wscan.sh
+++ wscan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SUDO_ASKPASS="$HOME/Scripts/askpass.sh"
-sudo -A iw wlp1s0 scan|grep SSID|awk '{print $2}'|tr -s "\n"|tr -s "\x00"|tr -s "\x0"|sort|uniq|dmenu|xargs -i x-terminal-emulator -e "echo 'enter pass:' && wpa_passphrase {}  | sudo -A tee -a /etc/wpa_supplicant/wpa_supplicant-wlp1s0.conf"
+SSID=$(sudo -A iw wlp1s0 scan|grep SSID|awk '{print $2}'|tr -s "\n"|sort|uniq|rofi -dmenu -only-match) && PASS=$(zenity --entry --text "Pass key for SSID: $SSID") && wpa_passphrase $SSID $PASS | sudo -A tee -a /etc/wpa_supplicant/wpa_supplicant-wlp1s0.conf
