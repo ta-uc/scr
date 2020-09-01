16d1221968aa3c4777f684d3489edf92f866472f
Takumi Uchida <mitauchiku@gmail.com>
2020-09-01 23:17:09 +0900

    mod: compatible with USBC port HDMI

--- display.sh
+++ display.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 PRIMARY_DISPLAY=$(xrandr | grep primary | awk '{print $1}')
+SECONDARY_DISPLAY=$(xrandr | grep " connected" | grep -v "primary" | awk '{print $1}')
+echo $SECONDARY_DISPLAY
 case $(echo -e "Main\nMirror\nExtend"|dmenu -i -p "Display?") in 
-  "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output HDMI2 --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output HDMI2 --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Extend") xrandr --output $PRIMARY_DISPLAY  --mode 1920x1080 --primary --output HDMI2 --auto --right-of $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
+  "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Extend") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --primary --output $SECONDARY_DISPLAY --auto --right-of $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
 esac
 
