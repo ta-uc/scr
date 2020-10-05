f34a2ec8346ae24ae4346fecfad40d1890583ac0
Takumi Uchida <mitauchiku@gmail.com>
2020-10-05 23:15:12 +0900

    mod: delete echo

--- display.sh
+++ display.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PRIMARY_DISPLAY=$(xrandr | grep primary | awk '{print $1}')
 SECONDARY_DISPLAY=$(xrandr | grep " connected" | grep -v "primary" | awk '{print $1}')
-echo $SECONDARY_DISPLAY
 case $(echo -e "Main\nMirror\nExtend"|dmenu -i -p "Display?") in 
   "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
     "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
