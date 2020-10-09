2e11cc53a885f98a8bf7c5d0bbf2c194f4e10433
Takumi Uchida <mitauchiku@gmail.com>
2020-10-09 20:55:24 +0900

    mod: wallpaper conf

--- display.sh
+++ display.sh
@@ -2,8 +2,8 @@
 PRIMARY_DISPLAY=$(xrandr | grep primary | awk '{print $1}')
 SECONDARY_DISPLAY=$(xrandr | grep " connected" | grep -v "primary" | awk '{print $1}')
 case $(echo -e "Main\nMirror\nExtend"|dmenu -i -p "Display?") in 
-  "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --off && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
-    "Extend") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --primary --output $SECONDARY_DISPLAY --auto --right-of $PRIMARY_DISPLAY && feh --bg-fill ~/Pictures/WP/w.jpg;;
+    "Main") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --off && feh --bg-fill $WP;;
+    "Mirror") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --primary --output $SECONDARY_DISPLAY --mode 1920x1080 --same-as $PRIMARY_DISPLAY && feh --bg-fill $WP;;
+    "Extend") xrandr --output $PRIMARY_DISPLAY --mode 1920x1080 --primary --output $SECONDARY_DISPLAY --auto --right-of $PRIMARY_DISPLAY && feh --bg-fill $WP
 esac
 
